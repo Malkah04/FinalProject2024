f3d5f93804e6f31caa512fb650e89be559abd45d
unknown <youstinaagaiby53@gmail.com>
2024-12-06 02:39:56 +0200

    updated by youstina

--- backend/models/Product.model.js
+++ backend/models/Product.model.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const productSchema =new Schema(
     {
-        productImage:{type : String, required: false },
+        productImage:{type : String, required: true },
         productName:{type : String, required: true, unique: true}, 
         productPrice:{type : String, required: true},
         productDescription:{type : String, required: true},
+        productCategory:{type : String, required: true},
         productQuantity:{type : String, required: true}
     }
 )
