9308f5f2ebd84af599f4ee31471e8f0191568a35
Roaa Ahmed <roaahamza01@gmail.com>
2024-12-07 04:28:48 +0200

    ubdate by roaa

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -7,19 +7,40 @@ import{
   Route,
   Routes
 } from 'react-router-dom';
+import About from './Components/About';
+import Privacy from './Components/Privacy';
+import Contact from './Components/Contact';
+import Feedback from './Components/feedback';
+import Backpack from './Components/Backpack';
+import Laptop from './Components/Laptop';
+import Crossbody from './Components/Crossbody';
+import Footer from './Components/Footer';
+import Main from './Components/Main';
+
 function App() {
   return (
     
     <>
     <Router>
-    
+       
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/posts" element={<Test />} />
-        {/* <Route path="/" element={<Main />} /> */}
+        <Route path="/" element={<Main />} /> 
+        <Route path="/about" element={<About />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/backpack" element={<Backpack />} />
+        <Route path="/laptop" element={<Laptop />} />
+        <Route path="/crossbody" element={<Crossbody />} />
         <Route path="*" element={<div>Page Not Found</div>} />
       </Routes>
+
+       
+      <Footer/>
+      
     </Router>
     </>
   );
