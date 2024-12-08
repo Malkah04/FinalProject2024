419729878509b267364a79cae55cbd15de4078b6
Rahma1604 <rahmamostafa041@gmail.com>
2024-12-08 23:24:32 +0200

    ubdate

--- frontend/my-app/src/Components/Privacy.js
+++ frontend/my-app/src/Components/Privacy.js
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import './Privacy.css';
 const Privacy = () => {
   return (
-    <div>Privacy</div>
+    <div className="privacy-container">
+      <h1> Privacy Policy </h1>
+       <h2>What Information DO We Collect And Store?</h2>
+       <h3>Personal Information</h3>
+      <p>
+      We don’t sell, trade, give away, or rent your personal information to any company
+We collect and store information that you provide to us, including through the Website or our Customer Service Department
+We will not disclose your personal information outside of adidas and the adidas Group without your consent.
+      </p>
+      <h3> The Information We Store </h3>
+    <p>
+    First and 
+    </p>
+    <p>Last name</p>
+   <p> Email Address</p>
+    <p>Mobile Number</p>
+   <p> Password</p>
+  
+    </div>
+
   )
 }
 
