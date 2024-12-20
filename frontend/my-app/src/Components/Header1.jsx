db3ea58cf5199fa5c18bb9278a0d56f0f0607377
Youstina Agaiby <youstinaagaiby53@gmail.com>
2024-12-20 20:23:36 +0200

    update by youstina

--- frontend/my-app/src/Components/Header1.jsx
+++ frontend/my-app/src/Components/Header1.jsx
@@ -1,84 +1,87 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Header1.css"; 
-
-
 import "./Header1.css";
 import PLogo from "./PLogo";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
   const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
- 
-  return (
-   
-      <header className="header">
-        <div className="header-logo">
-          <Link to="/" className="header-link">
-            <PLogo /> 
-            <span className="header-title"></span>
-          </Link>
-        </div>
-        <nav className="header-nav">
-        <div>
-            <Link to="/Home" className="header-nav-link">Home</Link>
-          </div>
-          <ul>
-
-            <li onClick={toggleDropdown}>
-          
-              Category
-              <span style={{ marginLeft: '5px' }}>&#9660;</span>
-
- 	   {isDropdownOpen && (
-              <ul >
-                <li >
-                  <Link to="/subcategory1">Subcategory 1</Link>
-                </li>
-                <li >
-                  <Link to="/subcategory2" >Subcategory 2</Link>
-                </li>
-                <li >
-                  <Link to="/subcategory3" >Subcategory 3</Link>
-                </li>
-            
-              </ul>
-            )}    
-            </li>
-            
-           
-          </ul>
-          
-          <div>
-            <Link to="/cart" className="header-nav-link">cart</Link>
-          </div>
-          <div>
-            <Link to="/login" className="header-nav-link">Login</Link>
-          </div>
-          <div>
-            <Link to="/account" className="header-nav-link">Account</Link>
-          </div>
-
-          <span className="header-profile">
-            <img src="search.png "  width="30"    alt="Profile Avatar" className="header-profile-img" /> 
-            </span>
-          <span className="header-profile">
-            <img src="cartbag.png "  width="40"    alt="Profile Avatar" className="header-profile-img" /> 
-            </span>
-          <span className="header-profile">
-            <img src="https://cdn2.iconfinder.com/data/icons/instagram-ui/48/jee-75-512.png "  width="50"    alt="Profile Avatar" className="header-profile-img" />           
-            {/* <div className="header-dropdown">
-              <Link to="/profile" className="header-dropdown-link">Profile</Link>
-              <Link to="/settings" className="header-dropdown-link">Settings</Link>
-              <Link to="/logout" className="header-dropdown-link">Logout</Link>
-            </div> */}
-          </span>
-        </nav>
-      </header>
-    );
+  const closeMenus = () => {
+    setIsMenuOpen(false);
+    setIsDropdownOpen(false);
   };
-  
- 
-export default Header;
 
+  return (
+    <header className="header">
+      <div className="header-logo">
+        <Link to="/" className="header-link">
+          <PLogo />
+          <span className="header-title"></span>
+        </Link>
+      </div>
+      <div className="menu-icon" onClick={toggleMenu}>
+        &#9776; 
+      </div>
+      <nav className={`header-nav ${isMenuOpen ? "active" : ""}`}>
+  <Link to="/Home" className="header-nav-link" onClick={closeMenus}>Home</Link>
+
+  <div className="dropdown">
+    <span className="header-nav-link" onClick={toggleDropdown}>
+      Category <span style={{ marginLeft: '5px' }}>&#9660;</span>
+    </span>
+    {isDropdownOpen && (
+      <ul className="dropdown-menu">
+        <li>
+          <Link to="/tote" className="dropdown-item" onClick={closeMenus}>Tote Bag</Link>
+        </li>
+        <li>
+          <Link to="/Backpack" className="dropdown-item" onClick={closeMenus}>Backpack</Link>
+        </li>
+        <li>
+          <Link to="/laptop" className="dropdown-item" onClick={closeMenus}>Laptop Bag</Link>
+        </li>
+      </ul>
+    )}
+  </div>
+
+  <Link to="/cart" className="header-nav-link" onClick={closeMenus}>Cart</Link>
+  <Link to="/login" className="header-nav-link" onClick={closeMenus}>Login</Link>
+  <Link to="/register" className="header-nav-link" onClick={closeMenus}>Sign up</Link>
+</nav>
+
+{isMenuOpen && (
+  <div className="sidebar-menu">
+    <Link to="/Home" className="sidebar-link" onClick={closeMenus}>Home</Link>
+
+    <div className="dropdown">
+      <span className="sidebar-link" onClick={toggleDropdown}>
+        Category <span style={{ marginLeft: '5px' }}>&#9660;</span>
+      </span>
+      {isDropdownOpen && (
+        <ul className="dropdown-menu">
+          <li>
+            <Link to="/tote" className="dropdown-item" onClick={closeMenus}>Tote Bag</Link>
+          </li>
+          <li>
+            <Link to="/Backpack" className="dropdown-item" onClick={closeMenus}>Back Bag</Link>
+          </li>
+          <li>
+            <Link to="/laptop" className="dropdown-item" onClick={closeMenus}>Laptop Bag</Link>
+          </li>
+        </ul>
+      )}
+    </div>
+
+    <Link to="/cart" className="sidebar-link" onClick={closeMenus}>Cart</Link>
+    <Link to="/login" className="sidebar-link" onClick={closeMenus}>Login</Link>
+    <Link to="/register" className="header-nav-link" onClick={closeMenus}>Sign up</Link>
+  </div>
+)}
+    </header>
+  );
+};
+
+export default Header;
